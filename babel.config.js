7de7795c5f5e78892ddbf17985f0b2480bb0444e
Paul Myburgh <paulmyburgh88@gmail.com>
2020-04-05 22:56:17 +0200

    removed unnecessary plugins

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  plugins: ["babel-plugin-inline-import"],
   env: {
     "server-dev": {
       presets: [
@@ -15,18 +14,7 @@ module.exports = {
         ],
         [
           "@babel/typescript",
-        ],
-        "@babel/react",
-      ],
-      plugins: [
-        "dynamic-import-node",
-        ["@babel/plugin-proposal-decorators", { "legacy": true }],
-        ["@babel/plugin-proposal-class-properties", { "loose" : true }],
-        "@babel/transform-react-constant-elements",
-        "@babel/transform-react-inline-elements",
-        "@babel/plugin-proposal-optional-chaining",
-        "@babel/plugin-proposal-nullish-coalescing-operator",
-        ["@babel/plugin-proposal-object-rest-spread", { useBuiltIns: true }],
+        ]
       ],
       sourceMaps: true,
       retainLines: true,
