e1a6a9984e2f9047f0f8af4f1e95ce952f13a30f
Paul Myburgh <paulmyburgh88@gmail.com>
2020-04-05 22:58:12 +0200

    simplified babel config further

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,19 @@
 module.exports = {
-  env: {
-    "server-dev": {
-      presets: [
-        [
-          "@babel/env",
-          {
-            loose: true,
-            modules: "commonjs",
-            targets: {
-              node: "current",
-            },
-          },
-        ],
-        [
-          "@babel/typescript",
-        ]
-      ],
-      sourceMaps: true,
-      retainLines: true,
-    },
-  },
+  presets: [
+    [
+      "@babel/env",
+      {
+        loose: true,
+        modules: "commonjs",
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+    [
+      "@babel/typescript",
+    ]
+  ],
+  sourceMaps: true,
+  retainLines: true,
 };
